04ab18ba29a557597c401012a1f62ae04d0d7d39
Gabriel-Henrique-Martins <80902575+Gabriel-Henrique-Martins@users.noreply.github.com>
2021-05-28 19:30:03 -0300

    aula2 javascript

---
+++ calculadora.js
@@ -0,0 +1,28 @@
+let numeroA = parseInt(prompt(`digite um numero`))  
+ 
+
+let sinaldaoperaçao = prompt(`digite um o sinal da operaçao`)
+
+let numeroC = parseInt(prompt(`digite outro numero`)) 
+
+
+//let resultado=0;
+ 
+if (sinaldaoperaçao === `+`)
+
+alert(`o resultado é ${numeroA + numeroC}`)
+
+else if (sinaldaoperaçao === `-`)
+alert(`o resultado é ${numeroA - numeroC}`)
+
+else if (sinaldeoperaçao === `*`)
+
+alert (`o resultado é ${numeroA * numeroC}`)
+
+else (sinaldeoperaçao === `/`)
+//alert (`o resultado é ${numeroA / numeroC}`)
+
+console.log(typeof(sinaldaoperaçao))
+console.log(typeof(numeroA))
+console.log(typeof(numeroC))
+
